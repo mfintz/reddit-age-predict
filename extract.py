fd2022b2af67d850cb47f4e8c13a69a90d735b5a
Matan Fintz <11562692+mfintz@users.noreply.github.com>
2018-03-20 13:51:31 +0200

    added praw reddit object

---
+++ extract.py
@@ -0,0 +1,24 @@
+import praw
+import codecs
+
+
+
+# Make a new file
+output = codecs.open("out.txt", 'w', "utf-8")
+
+
+reddit = praw.Reddit(client_id='E8sXdgKwMzysNQ',
+                     client_secret='Bfixm1o5wBTV8hPX8Pc5HVNN7Qg', password='nlpredditproject',
+                     user_agent='Nlproject', username='nlpredditproject')
+
+
+subreddit = reddit.subreddit('teenagers')
+top_subreddit = subreddit.hot(limit = 9999)
+for index,submission in enumerate(top_subreddit):
+    output.write(str(index))
+    output.write(":")
+    output.write(submission.title)
+    output.write("\n")
+
+
+output.close()
